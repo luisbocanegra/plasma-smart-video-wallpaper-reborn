aff251a7089cb9856611963b3c0c1a15a03263fe
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2026-02-01 14:52:08 -0600

    fix: inform about KDE Store version removal on install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ cmake -B build -S .
 cmake --build build
 sudo cmake --install build
 
-# remove KDE Store / kpackagetool6 install so it doesn't override the system one
-
-rm -r "$HOME/.local/share/plasma/wallpapers/luisbocanegra.smart.video.wallpaper.reborn/"
+# remove KDE Store / kpackagetool6 install so it doesn't override the system-wide one
+echo "Removing previous install (if exists) from $HOME/.local/share/plasma/wallpapers/"
+rm -r "$HOME/.local/share/plasma/wallpapers/luisbocanegra.smart.video.wallpaper.reborn/" 2>/dev/null || true
+echo "Done"
